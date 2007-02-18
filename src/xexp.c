63e0e08e637b53fd93139ada199fc34284c43fe5
mvo <mvo@e55152ac-3a23-0410-980c-f88739b87167>
2007-02-18 04:48:33 +0000

    Fleshed out.
    git-svn-id: https://garage.maemo.org/svn/hildon-app-mgr/trunk@126 e55152ac-3a23-0410-980c-f88739b87167

--- src/xexp.c
+++ src/xexp.c
@@ -21,18 +21,20 @@
  *
  */
 
+#include <glib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+#include <unistd.h>
+
 #include "xexp.h"
 
-typedef enum {
-  XEXP_TAG,
-  XEXP_TEXT
-} xexp_type;
-
 struct xexp {
-  xexp_type type;
-  const char *string;
-  xexp *first;
+  char *tag;
   xexp *rest;
+  xexp *first;
+  char *text;
 };
 
 xexp *
@@ -42,20 +44,15 @@ xexp_rest (xexp *x)
 }
 
 void
-xexp_set_rest (xexp *x, xexp *rest)
-{
-  x->rest = rest;
-}
-
-void
 xexp_free_1 (xexp *x)
 {
-  g_free (x->string);
+  g_free (x->tag);
+  g_free (x->text);
   g_free (x);
 }
 
 void
-xexp_free (xexp *x);
+xexp_free (xexp *x)
 {
   while (x)
     {
@@ -66,80 +63,474 @@ xexp_free (xexp *x);
     }
 }
 
+const char *
+xexp_tag (xexp *x)
+{
+  return x->tag;
+}
+
+int
+xexp_is (xexp *x, const char *tag)
+{
+  return strcmp (x->tag, tag) == 0;
+}
+
 xexp *
-xexp_tag_new (const char *tag, xexp *first, xexp *rest)
+xexp_list_new (const char *tag, xexp *first, xexp *rest)
 {
   g_assert (tag);
 
   xexp *x = g_new0 (xexp, 1);
-  x->type = XEXP_TAG;
-  x->string = tag;
+  x->tag = g_strdup (tag);
+  x->text = NULL;
   x->first = first;
   x->rest = rest;
   return x;
 }
 
-bool
-xexp_is_tag (xexp *x)
+int
+xexp_is_list (xexp *x)
 {
-  return x->type == XEXP_TAG;
-}
-
-const char *
-xexp_get_tag (xexp *x)
-{
-  g_assert (xexp_is_tag (x));
-  return x->string;
-}
-
-bool
-xexp_is (xexp *x, const char *tag)
-{
-  return xexp_is_tag (x) && strcmp (xexp_get_tag (x), tag) == 0;
+  return x->text == NULL;
 }
 
 xexp *
 xexp_first (xexp *x)
 {
-  g_assert (xexp_is_tag (x));
+  g_assert (xexp_is_list (x));
   return x->first;
 }
 
 void
-xexp_set_first (xexp *x, xexp *first)
+xexp_prepend (xexp *x, xexp *y)
 {
-  g_assert (xexp_is_tag (x));
-  x->first = first;
+  xexp **yptr;
+  g_assert (xexp_is_list (x));
+  
+  yptr = &y->rest;
+  while (*yptr)
+    yptr = &(*yptr)->rest;
+  *yptr = x->first;
+  x->first = y;
+}
+
+void
+xexp_append (xexp *x, xexp *y)
+{
+  xexp **yptr;
+  g_assert (xexp_is_list (x));
+
+  yptr = &x->first;
+  while (*yptr)
+    yptr = &(*yptr)->rest;
+  *yptr = y;
+}
+
+void
+xexp_del (xexp *x, xexp *z)
+{
+  xexp **yptr;
+
+  g_assert (xexp_is_list (x));
+  yptr = &x->first;
+  while (*yptr)
+    {
+      xexp *y = *yptr;
+      if (y == z)
+	{
+	  *yptr = y->rest;
+	  y->rest = NULL;
+	  xexp_free (y);
+	  return;
+	}
+      yptr = &(*yptr)->rest;
+    }
+}
+
+void
+xexp_set_rest (xexp *x, xexp *y)
+{
+  xexp_free (x->rest);
+  x->rest = y;
+}
+
+int
+xexp_length (xexp *x)
+{
+  int len;
+  xexp *y;
+
+  g_assert (xexp_is_list (x));
+  for (len = 0, y = xexp_first (x); y; len++, y = xexp_rest (y))
+    ;
+  return len;
 }
 
 xexp *
-xexp_text_new (const char *text, xexp *rest)
+xexp_text_new (const char *tag, const char *text, xexp *rest)
 {
+  g_assert (tag);
   g_assert (text);
 
   xexp *x = g_new0 (xexp, 1);
-  x->type = XEXP_TEXT;
-  x->string = text;
+  x->tag = g_strdup (tag);
+  x->text = g_strdup (text);
   x->first = NULL;
   x->rest = rest;
   return x;
 }
 
-bool
+int
 xexp_is_text (xexp *x)
 {
-  return x->type == XEXP_TEXT;
+  return x->text != NULL;
 }
 
 const char *
-xexp_get_text (xexp *x)
+xexp_text (xexp *x)
 {
   g_assert (xexp_is_text (x));
-  return x->string;
+  return x->text;
 }
 
 int
-xexp_get_text_as_int (xexp *x)
+xexp_text_as_int (xexp *x)
 {
-  return strtod (xexp_get_text (x));
+  return atoi (xexp_text (x));
+}
+
+xexp *
+xexp_aref (xexp *x, const char *tag)
+{
+  xexp *y = xexp_first (x);
+  while (y)
+    {
+      if (xexp_is (y, tag))
+	return y;
+      y = xexp_rest (y);
+    }
+  return NULL;
+}
+
+const char *
+xexp_aref_text (xexp *x, const char *tag)
+{
+  xexp *y = xexp_aref (x, tag);
+  if (y && xexp_is_text (y))
+    return xexp_text (y);
+  else
+    return NULL;
+}
+
+int
+xexp_aref_bool (xexp *x, const char *tag)
+{
+  return xexp_aref (x, tag) != NULL;
+}
+
+void
+xexp_aset (xexp *x, xexp *val)
+{
+  xexp_adel (x, xexp_tag (val));
+  xexp_prepend (x, val);
+}
+
+void
+xexp_aset_bool (xexp *x, const char *tag, int val)
+{
+  if (val)
+    xexp_aset (x, xexp_list_new (tag, NULL, NULL));
+  else
+    xexp_adel (x, tag);
+}
+
+void
+xexp_aset_text (xexp *x, const char *tag, const char *val)
+{
+  if (val)
+    xexp_aset (x, xexp_text_new (tag, val, NULL));
+  else
+    xexp_adel (x, tag);
+}
+
+void
+xexp_adel (xexp *x, const char *tag)
+{
+  xexp **yptr;
+
+  g_assert (xexp_is_list (x));
+  yptr = &x->first;
+  while (*yptr)
+    {
+      xexp *y = *yptr;
+      if (xexp_is (y, tag))
+	{
+	  *yptr = y->rest;
+	  y->rest = NULL;
+	  xexp_free (y);
+	  return;
+	}
+      yptr = &(*yptr)->rest;
+    }
+}
+
+void
+transmogrify_text_to_list (xexp *x)
+{
+  g_free (x->text);
+  x->text = NULL;
+}
+
+void
+transmogrify_list_to_text (xexp *x, const char *text)
+{
+  g_assert (text);
+  g_assert (x->first == NULL);
+  x->text = g_strdup (text);
+}
+
+/** Parsing */
+
+typedef struct {
+  xexp *result;
+  GSList *stack;
+} xexp_parse_context;
+
+static void
+ignore_text (const char *text, GError **error)
+{
+  const char *p = text;
+  while (*p && isspace (*p))
+    p++;
+
+  if (*p)
+    g_set_error (error, 
+		 G_MARKUP_ERROR,
+		 G_MARKUP_ERROR_INVALID_CONTENT,
+		 "Unexpected text: %s", text);
+}
+
+static void
+xexp_parser_start_element (GMarkupParseContext *context,
+			   const gchar         *element_name,
+			   const gchar        **attribute_names,
+			   const gchar        **attribute_values,
+			   gpointer             user_data,
+			   GError             **error)
+{
+  xexp_parse_context *xp = (xexp_parse_context *)user_data;
+
+  xexp *x = xexp_list_new (element_name, NULL, NULL);
+  if (xp->stack)
+    {
+      /* If the current node is a text, it must be all whitespace and
+	 we turn it into a list node.
+      */
+      xexp *y = (xexp *)xp->stack->data;
+      if (xexp_is_text (y))
+	{
+	  ignore_text (xexp_text (y), error);
+	  transmogrify_text_to_list (y);
+	}
+
+      xexp_append (y, x);
+    }
+  xp->stack = g_slist_prepend (xp->stack, x);
+}
+
+static void
+xexp_parser_end_element (GMarkupParseContext *context,
+			 const gchar         *element_name,
+			 gpointer             user_data,
+			 GError             **error)
+{
+  xexp_parse_context *xp = (xexp_parse_context *)user_data;
+
+  GSList *top = xp->stack;
+  xexp *x = (xexp *)top->data;
+  xp->stack = top->next;
+  g_slist_free_1 (top);
+
+  if (xp->stack == NULL)
+    xp->result = x;
+}
+
+static void
+xexp_parser_text (GMarkupParseContext *context,
+		  const gchar         *text,
+		  gsize                text_len,
+		  gpointer             user_data,
+		  GError             **error)
+{
+  xexp_parse_context *xp = (xexp_parse_context *)user_data;
+
+  /* If the current xexp is has already children, TEXT must be all
+     whitespace and we ignore it.  Otherwise, we turn the current node
+     into text.
+  */
+  
+  if (xp->stack)
+    {
+      xexp *y = (xexp *)xp->stack->data;
+      if (xexp_first (y))
+	ignore_text (text, error);
+      else
+	transmogrify_list_to_text (y, text);
+    }
+}
+
+static GMarkupParser xexp_markup_parser = {
+  xexp_parser_start_element,
+  xexp_parser_end_element,
+  xexp_parser_text,
+  NULL,
+  NULL
+};
+
+xexp *
+xexp_read (FILE *f, GError **error)
+{
+  xexp_parse_context xp;
+  GMarkupParseContext *ctxt;
+  gchar buf[1];
+  size_t n;
+
+  xp.stack = NULL;
+  xp.result = NULL;
+  ctxt = g_markup_parse_context_new (&xexp_markup_parser, 0, &xp, NULL);
+
+  while (xp.result == NULL && (n = fread (buf, 1, 1, f)) > 0)
+    {
+      if (!g_markup_parse_context_parse (ctxt, buf, n, error))
+	goto error;
+    }
+
+  if (!g_markup_parse_context_end_parse (ctxt, error))
+    goto error;
+    
+
+  g_assert (xp.result && xp.stack == NULL);
+		   
+  return xp.result;
+
+ error:
+  if (xp.stack)
+    {
+      xexp *top = (xexp *) g_slist_last (xp.stack)->data;
+      xexp_free (top);
+    }
+  else if (xp.result)
+    xexp_free (xp.result);
+  return NULL;
+}
+
+xexp *
+xexp_read_file (const char *filename)
+{
+  FILE *f = fopen (filename, "r");
+  if (f)
+    {
+      GError *error = NULL;
+      xexp *x = xexp_read (f, &error);
+      fclose (f);
+      if (error)
+	{
+	  fprintf (stderr, "%s: %s\n", filename, error->message);
+	  g_error_free (error);
+	}
+      return x;
+    }
+  fprintf (stderr, "%s: %s\n", filename, strerror (errno));
+  return NULL;
+}
+
+/** Writing */
+
+static void
+xexp_fprintf_escaped (FILE *f, const char *fmt, ...)
+{
+  va_list args;
+  char *str;
+
+  va_start (args, fmt);
+  str = g_markup_vprintf_escaped (fmt, args);
+  va_end (args);
+  fputs (str, f);
+  g_free (str);
+}
+
+static void
+write_blanks (FILE *f, int level)
+{
+  static const char blanks[] = "                ";
+  if (level > sizeof(blanks)-1)
+    level = sizeof(blanks)-1;
+  fputs (blanks + sizeof(blanks)-1 - level, f);
+}
+
+static void
+xexp_write_1 (FILE *f, xexp *x, int level)
+{
+  xexp *y;
+
+  write_blanks (f, level);
+
+  if (xexp_is_list (x))
+    {
+      y = xexp_first (x);
+      if (y == NULL)
+	xexp_fprintf_escaped (f, "<%s/>\n", xexp_tag (x));
+      else 
+	{
+	  xexp_fprintf_escaped (f, "<%s>\n", xexp_tag (x));
+	  for (; y; y = xexp_rest (y))
+	    xexp_write_1 (f, y, level + 1);
+	  write_blanks (f, level);
+	  xexp_fprintf_escaped (f, "</%s>\n", xexp_tag (x));
+	}
+    }
+  else
+    xexp_fprintf_escaped (f, "<%s>%s</%s>\n",
+			  xexp_tag (x), xexp_text (x), xexp_tag (x));
+}
+
+void
+xexp_write (FILE *f, xexp *x)
+{
+  xexp_write_1 (f, x, 0);
+}
+
+int
+xexp_write_file (const char *filename, xexp *x)
+{
+  char *tmp_filename = g_strdup_printf ("%s#%d", filename, getpid);
+  FILE *f = fopen (tmp_filename, "w");
+
+  if (f == NULL)
+    goto error;
+
+  xexp_write (f, x);
+  if (ferror (f))
+    goto error;
+
+  if (fclose (f) < 0)
+    {
+      f = NULL;
+      goto error;
+    }
+  f = NULL;
+
+  if (rename (tmp_filename, filename) < 0)
+    goto error;
+  
+  g_free (tmp_filename);
+  return 1;
+
+ error:
+  fprintf (stderr, "%s: %s\n", filename, strerror (errno));
+  if (f)
+    fclose (f);
+  if (tmp_filename != filename)
+    g_free (tmp_filename);
+  return 0;
 }
--- src/xexp.h
+++ src/xexp.h
@@ -21,62 +21,103 @@
  *
  */
 
-/* S-expression like things encoded with XML.  This is used for
-   various configuration files and the installation instructions.
+/* X-expressions are a bit like S-expression, but suitable for
+   encoding in XML.  They are used for various configuration files and
+   the installation instructions.
 
-   A X-expression, or xexp for short, is either a 'tag' or 'text'.
-   Tags have a symbolic tag and children.  A text is just a string.
-   There are utility functions for treating texts as integers, but all
-   types must be inferred from the context.
-
-   All strings must be in UTF-8.  In the list of children of a tag
-   node, no text node can follow another text node.
+   An X-expression, or xexp for short, has a 'tag' and is either a
+   'list' or 'text'.  Lists have children and text xexps have a
+   string.  There are utility functions for treating texts as
+   integers, but all types must be inferred from the context.
 
    The external representation of a xexp uses the subset of XML
    supported by the glib XML parser.  For a text node, the text is
-   written with all the necessary escapes.  For a tag node, the
-   format is
+   written with all the necessary escapes between its tag:
+
+       <TAG>TEXT</TAG>
+
+   For a list xexp, the format is
    
        <TAG>CHILD1 CHILD2 ...</TAG>
 
-   with no space between the representations of the children.  If the
-   node has no has children, an alternative encoding is
+   with no space between the representations of the children.
+
+   The ambiguity between text xexps with an empty string and empty
+   lists is resolved by writing empty strings as
+
+       <TAG></TAG>
+
+   and empty lists as
 
        <TAG/>
+
+   NULL is not a valid xexp, but NULL is used to designate the end of
+   the list of children.
+
+   Using lists and texts, some 'higher-level' constructs are defined:
+   a association list is a list that is meant to be used as a
+   dictionary using xexp_aref.  An empty list is often used as a flag
+   whose presence or absence in a association list is used to encode a
+   boolean value.
+
+   Memory management is very simplistic: X-expression can not share
+   structure or form cycles.  Every xexp is referenced by at most one
+   other xexp, and when that references is broken, the xexp is freed.
+   In other words, as soon as you put a xexp into a list, the list
+   assumes ownership of that xexp.
 */
 
 #ifndef XEXP_H
 #define XEXP_H
 
 #include <stdio.h>
+#include <glib.h>
 
 struct xexp;
 typedef struct xexp xexp;
 
+/* General
+ */
+const char *xexp_tag (xexp *x);
+int xexp_is (xexp *x, const char *tag);
 xexp *xexp_rest (xexp *x);
-void xexp_set_rest (xexp *x, xexp *rest);
+void xexp_set_rest (xexp *x, xexp *y);
 void xexp_free_1 (xexp *x);
 void xexp_free (xexp *x);
 
-/* Tags
+/* Lists
  */
-xexp *xexp_tag_new (const char *tag, xexp *first, xexp *rest);
-bool xexp_is_tag (xexp *x);
-const char *xexp_get_tag (xexp *x);
-bool xexp_is (xexp *x, const char *tag);
+xexp *xexp_list_new (const char *tag, xexp *first, xexp *rest);
+int xexp_is_list (xexp *x);
 xexp *xexp_first (xexp *x);
-void xexp_set_first (xexp *x, xexp *first);
+int xexp_length (xexp *x);
+void xexp_prepend (xexp *x, xexp *y);
+void xexp_append (xexp *x, xexp *y);
+void xexp_del (xexp *x, xexp *y);
 
 /* Texts
  */
-xexp *xexp_text_new (const char *text, xexp *rest);
-bool xexp_is_text (xexp *x);
-const char *xexp_get_text (xexp *x);
-int xexp_get_text_as_int (xexp *x);
+xexp *xexp_text_new (const char *tag, const char *text, xexp *rest);
+int xexp_is_text (xexp *x);
+const char *xexp_text (xexp *x);
+int xexp_text_as_int (xexp *x);
+
+/* Association lists
+ */
+xexp *xexp_aref (xexp *x, const char *tag);
+const char *xexp_aref_text (xexp *x, const char *tag);
+int xexp_aref_bool (xexp *x, const char *tag);
+void xexp_aset (xexp *x, xexp *val);
+void xexp_aset_text (xexp *x, const char *tag, const char *val);
+void xexp_aset_bool (xexp *x, const char *tag, int val);
+void xexp_adel (xexp *x, const char *tag);
 
 /* Reading and writing
  */
-xexp *xexp_read (FILE *f, const char *line_prefix);
-void xexp_write (FILE *f, xexp *x, const char *line_prefix);
+xexp *xexp_read (FILE *f, GError **error);
+void xexp_write (FILE *f, xexp *x);
+
+xexp *xexp_read_file (const char *filename);
+int xexp_write_file (const char *filename, xexp *x);
 
 #endif
