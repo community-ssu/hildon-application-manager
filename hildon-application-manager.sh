bef8a4646e2ca95921c78a329cc04337cfb82714
mvo <mvo@e55152ac-3a23-0410-980c-f88739b87167>
2007-05-09 18:07:08 +0000

    Do not invoke AM, BAckup does it already at the right time.
    git-svn-id: https://garage.maemo.org/svn/hildon-app-mgr/trunk@385 e55152ac-3a23-0410-980c-f88739b87167

--- hildon-application-manager.sh
+++ hildon-application-manager.sh
@@ -5,11 +5,6 @@
 # We merge the restored catalogues right here and copy the list of
 # packages to a place where the AM can find them.
 
-if [ ! $1 ]
-then
-  exit 0;
-fi
-
 if [ ! -e $1 ]
 then
   exit 0
@@ -27,7 +22,6 @@ fi
 if grep -q "$pkgs" "$1"
 then
   cp "$pkgs" "$upkgs"
-  hildon-application-manager-util run-after-restore
 fi
 
 exit 0
