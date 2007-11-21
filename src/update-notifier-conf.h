319ca568e55afa8bacc2454a95875e766f0ddca6
mvo <mvo@e55152ac-3a23-0410-980c-f88739b87167>
2007-11-21 15:23:05 +0000

    * src/update-notifier-conf.h: New, to collect bits that are of 	interest to all parties.
    git-svn-id: https://garage.maemo.org/svn/hildon-app-mgr/trunk@824 e55152ac-3a23-0410-980c-f88739b87167

---
+++ src/update-notifier-conf.h
@@ -0,0 +1,38 @@
+/*
+ * This file is part of the hildon-application-manager.
+ *
+ * Copyright (C) 2007 Nokia Corporation.  All Rights reserved.
+ *
+ * Contact: Marius Vollmer <marius.vollmer@nokia.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+#ifndef UPDATE_NOTIFIER_CONF_H
+#define UPDATE_NOTIFIER_CONF_H
+
+#define UPNO_GCONF_DIR            "/apps/hildon/update-notifier"
+#define UPNO_GCONF_STATE          UPNO_GCONF_DIR "/state"
+#define UPNO_GCONF_ALARM_COOKIE   UPNO_GCONF_DIR "/alarm_cookie"
+#define UPNO_GCONF_CHECK_INTERVAL UPNO_GCONF_DIR "/check_interval"
+#define UPNO_GCONF_LAST_UPDATE    UPNO_GCONF_DIR "/last_update"
+
+#define UPNO_DEFAULT_CHECK_INTERVAL (24*60)
+
+#define SEEN_UPDATES_FILE  ".hildon-application-manager-seen-updates"
+#define AVAILABLE_UPDATES_FILE "/var/lib/hildon-application-manager/available-updates"
+
+#endif /* !UPDATE_NOTIFIER_CONF_H */
