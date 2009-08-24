ec6963e4fcd8c8fe019b119d563c23d0bb5cfd89
Mario Sanchez Prada <msanchez@igalia.com>
2009-08-24 13:05:07 +0200

    Remove user files when clearing the device (NB#128487)
    * ham-clean.sh: Explicitly remove every file under
      /home/user/.hildon.application-manager when clearing the device.

--- ham-clean.sh
+++ ham-clean.sh
@@ -3,4 +3,16 @@
 # Clear user defined catalogues
 sudo /usr/bin/hildon-application-manager-util clear-user-catalogues
 
+# Clear user files for HAM
+HAM_UFILES_DIR=/home/user/.hildon-application-manager
+rm -f "${HAM_UFILES_DIR}/packages.backup"
+rm -f "${HAM_UFILES_DIR}/seen-updates"
+rm -f "${HAM_UFILES_DIR}/tapped-updates"
+rm -f "${HAM_UFILES_DIR}/seen-notifications"
+rm -f "${HAM_UFILES_DIR}/tapped-notifications"
+rm -f "${HAM_UFILES_DIR}/available-notifications"
+rm -f "${HAM_UFILES_DIR}/boot"
+rm -f "${HAM_UFILES_DIR}/update-notifier"
+rm -f "${HAM_UFILES_DIR}/last-update"
+
 exit 0
