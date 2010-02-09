55b8f4dfedb0bd5101868f80afedae72f983079f
Mario Sanchez Prada <msanchez@igalia.com>
2010-02-09 10:15:08 +0100

    Make sure packages list is always initialized after restoring
    * src/main.cc (rp_end): Make sure packages list is always initialized
    after restoring the list of packages.

--- src/main.cc
+++ src/main.cc
@@ -2402,6 +2402,9 @@ rp_end (int n_successful, void *data)
     xexp_free (backup);
 
   end_interaction_flow ();
+
+  /* Make sure packages list is initialized after this */
+  maybe_init_packages_list ();
 }
 
 /* INSTALL_FROM_FILE_FLOW
